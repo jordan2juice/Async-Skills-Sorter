e6486b27ab3de4335d749a5566d31291b55047ed
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2024-02-09 14:51:54 -0600

    made changes to js

--- super.js
+++ super.js
@@ -1,28 +1,17 @@
-"use strick";
+"use strict";
 
-const superHero = "superhero.json";
+// const superHero = "superhero.json";
 
 // Fetch the JSON file
-async function getData(superHero) {
-  const response = await fetch(superHero);
-  const data = await response.json();
-  const sortedData = data.sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
-  });
-
-  displayCharacters(sortedData);
-
-  getData(superHero);
-
-  // Display the sorted data in the <ul> element
-  const list = document.getElementById("superhero-list");
-  data.forEach((superhero) => {
-    const li = document.createElement("li");
-    li.textContent = `${superhero.name}`;
-    list.appendChild(li);
-  });
+async function getData() {
+  try {
+    const data = await fetch("superhero.json");
+    const newData = await data.json;
+    console.log(newData);
+    const sortData = newData.sort((a, b) => (a.level > b.level ? -1 : 1));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getData();
